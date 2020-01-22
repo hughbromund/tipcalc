bf23a062c544d02ff41cdfc2e9bd55e718912953
hugh <hughbromund44@gmail.com>
2020-01-22 18:33:02 -0500

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Hugh Bromund on 1/22/20.
+//  Copyright © 2020 Hugh Bromund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
